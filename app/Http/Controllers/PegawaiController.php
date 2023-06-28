fa645727a89183a4a9e0db5b6c3818a4b9a28262
nadya <nadndyaa@gmail.com>
2023-06-28 22:07:54 +0700

    updt

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -8,16 +8,5 @@ use Illuminate\Support\Facades\DB;
 class PegawaiController extends Controller
 {
     //
-    public function pembelian2(){ 
-        return view('layouts.pembelian2');
-    }
-
-    public function rekappembelian2()
-    {
-        $rekapPembelian = Pembelian::select('id_member', 'id_pembelian', \DB::raw('count(*) as jumlah_pembelian'))
-            ->groupBy('id_member', 'id_pembelian')
-            ->get();
-
-        return view('layouts.rekap-pembelian2', compact('rekapPembelian'));
-    }
+   
 }
